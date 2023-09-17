94fcd9ef7863abc133abd1c71238bc27eb81a7ee
Lightning8 <tov.kessler@yandex.ru>
2023-09-17 18:07:25 +0300

    feat: add bacis highlighting

--- Assets/Scripts/Chessboard.cs
+++ Assets/Scripts/Chessboard.cs
@@ -8,7 +8,8 @@ public class Chessboard : MonoBehaviour
   private const int TILE_COUNT_X = 8;
   private const int TILE_COUNT_Y = 8;
   private GameObject[,] tiles;
-
+  private Camera currentCamera;
+  private Vector2Int currentHover;
 
   // LOGIC
   // Init function 
@@ -17,6 +18,48 @@ public class Chessboard : MonoBehaviour
     GenerateAllTiles(1, TILE_COUNT_X, TILE_COUNT_Y);
   }
 
+  private void Update()
+  {
+    if (!currentCamera)
+    {
+      currentCamera = Camera.main;
+      return;
+    }
+
+    // Highlighting tiles
+    RaycastHit info;
+    Ray ray = currentCamera.ScreenPointToRay(Input.mousePosition);
+  
+    if (Physics.Raycast(ray, out info, 100, LayerMask.GetMask("Tile", "Hover")))
+    {
+      // Get the indexes of the tile i've hit
+      Vector2Int hitPosition = LookupTileIndex(info.transform.gameObject);
+
+      // If we're hovering a tile after not hovering any tiles
+      if (currentHover == -Vector2Int.one)
+      {
+        currentHover = hitPosition;
+        tiles[hitPosition.x, hitPosition.y].layer = LayerMask.NameToLayer("Hover");
+      }
+
+      // If we were already hovering a tile, change the previous one
+      if (currentHover != hitPosition)
+      {
+        tiles[currentHover.x, currentHover.y].layer = LayerMask.NameToLayer("Tile");
+        currentHover = hitPosition;
+        tiles[hitPosition.x, hitPosition.y].layer = LayerMask.NameToLayer("Hover");
+      }
+    }
+    else
+    {
+      if (currentHover != -Vector2Int.one)
+      {
+        tiles[currentHover.x, currentHover.y].layer = LayerMask.NameToLayer("Tile");
+        currentHover = -Vector2Int.one;
+      }
+    }
+  }
+  // Generate the board
   private void GenerateAllTiles(float tileSize, int tileCountX, int tileCountY)
   {
     tiles = new GameObject[tileCountX, tileCountY];
@@ -47,15 +90,34 @@ public class Chessboard : MonoBehaviour
     vertices[2] = new Vector3((x + 1) * tileSize, 0, y * tileSize);
     vertices[3] = new Vector3((x + 1) * tileSize, 0, (y + 1) * tileSize);
 
-    int[] tris = new int[] { 0, 1, 2, 1, 3, 2};
+    int[] tris = new int[] { 0, 1, 2, 1, 3, 2 };
 
     mesh.vertices = vertices;
     mesh.triangles = tris;
-
     mesh.RecalculateNormals();
 
+    tileObject.layer = LayerMask.NameToLayer("Tile");
     tileObject.AddComponent<BoxCollider>();
 
     return tileObject;
   }
+
+  // Operations
+  private Vector2Int LookupTileIndex(GameObject hitInfo)
+  {
+    for (int x = 0; x < TILE_COUNT_X; x++)
+    {
+      for (int y = 0; y < TILE_COUNT_Y; y++)
+      {
+        if (tiles[x, y] == hitInfo)
+        {
+          return new Vector2Int(x, y);
+        }
+      }
+    }
+
+    return -Vector2Int.one; // Invalid
+  }
+
+
 }
